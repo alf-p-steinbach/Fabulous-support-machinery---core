9cbcbb5be849fb7361ce88f58e31e9963448c9cb
alf-p-steinbach <alf.p.steinbach@gmail.com>
2023-04-14 10:51:25 +0200

    Update Polymorphic.hpp

--- source/library/core/exports/meta-type/class-kinds/Polymorphic.hpp
+++ source/library/core/exports/meta-type/class-kinds/Polymorphic.hpp
@@ -1,8 +1,6 @@
 ﻿#pragma once    // Source encoding: UTF-8 with BOM (π is a lowercase Greek "pi").
 #include <fsm/core/exports/+std-cpp-language.hpp>
 
-#include <fsm/core/exports/constructs/type_builders.hpp>        // in_
-
 namespace fabulous_support_machinery {
 
     struct Polymorphic
