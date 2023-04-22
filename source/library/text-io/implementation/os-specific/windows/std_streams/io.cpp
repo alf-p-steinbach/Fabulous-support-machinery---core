ef506e823539e30a0ef9d753085e6b53ef45dd6d
alf-p-steinbach <alf.p.steinbach@gmail.com>
2023-04-22 16:58:23 +0200

    Update io.cpp

--- source/library/text-io/implementation/os-specific/windows/std_streams/io.cpp
+++ source/library/text-io/implementation/os-specific/windows/std_streams/io.cpp
@@ -3,7 +3,7 @@
 
 #include <fsm/text-io/exports/std_streams/io.hpp>       // ~
 
-#include <fsm/core/exports/constructs/declarations/type_builders.hpp>                    // const_, in_
+#include <fsm/core/exports/constructs/declarations/type_builders.hpp>       // const_, in_
 #include <fsm/core/exports/failure/detecting/hopefully.hpp>                 // hopefully
 #include <fsm/core/exports/failure/expressing/FSM_FAIL.hpp>                 // FSM_FAIL
 #include <fsm/core/exports/support-for-collections/size-functions.hpp>      // int_size_of, is_empty
@@ -16,6 +16,27 @@
 namespace fabulous_support_machinery::std_streams::_definitions {
     using   std::move;          // <utility>
 
+    static auto stdin_input_using_buffer( string&& buffer )
+        -> string
+    {
+        buffer.clear();
+        for( ;; ) {
+            const int code = getchar();
+            if( code == EOF ) {
+                // Note: `getchar` return value EOF also covers the case of error.
+                hopefully( feof( stdin ) )
+                    or FSM_FAIL( "The standard input stream failed (C ferror is true)" );
+                hopefully( not is_empty( buffer ) )
+                    or FSM_FAIL_WITH_DEFAULT_MESSAGE_( x::End_of_file );
+                break;
+            } else if( code == '\n' ) {
+                break;
+            }
+            buffer += char( code );
+        }
+        return move( buffer );
+    }
+    
     auto input_using_buffer( string&& buffer )
         -> string
     {
@@ -23,25 +44,10 @@ namespace fabulous_support_machinery::std_streams::_definitions {
             // // TODO: return console::input_using_buffer( move( buffer ) );
             // return "";
         // } else {
-            buffer.clear();
-            for( ;; ) {
-                const int code = getchar();
-                if( code == EOF ) {
-                    // Note: `getchar` return value EOF also covers the case of error.
-                    hopefully( feof( stdin ) )
-                        or FSM_FAIL( "The standard input stream failed (C ferror is true)" );
-                    hopefully( not is_empty( buffer ) )
-                        or FSM_FAIL_WITH_DEFAULT_MESSAGE_( x::End_of_file );
-                    break;
-                } else if( code == '\n' ) {
-                    break;
-                }
-                buffer += char( code );
-            }
-            return move( buffer );
+            return stdin_input_using_buffer( move( buffer ) );
         // }
     }
-    
+
     void output_to( const Output_stream_id stream_id, in_<string_view> s )
     {
         if( is_console_stream( stream_id ) ) {
