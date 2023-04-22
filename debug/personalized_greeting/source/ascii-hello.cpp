b4d15c5c8cf36fda0776830b4aa66ded3d23e24e
alf-p-steinbach <alf.p.steinbach@gmail.com>
2023-04-23 00:04:20 +0200

    Delete debug/personalized_greeting/source directory

--- debug/personalized_greeting/source/ascii-hello.cpp
+++
@@ -1,8 +0,0 @@
-#include <fsm/text-io.hpp>
-namespace fsm = fabulous_support_machinery;
-
-auto main() -> int
-{
-    using fsm::console::output;
-    output( "The Fabulous Support Machinery (FSM) library says hello!\n" );
-}
--- debug/personalized_greeting/source/unicode-hello.cpp
+++
@@ -1,8 +0,0 @@
-﻿#include <fsm/text-io.hpp>
-namespace fsm = fabulous_support_machinery;
-
-auto main() -> int
-{
-    using fsm::console::output;
-    output( "Did you know, every 日本国 кошка loves blåbærsyltetøy?\n" );
-}
