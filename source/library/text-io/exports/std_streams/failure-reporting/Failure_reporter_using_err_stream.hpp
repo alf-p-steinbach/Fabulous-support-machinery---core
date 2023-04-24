a9cc40633d5ad4b803930ad5e11b41bdcde15673
alf-p-steinbach <alf.p.steinbach@gmail.com>
2023-04-24 23:06:13 +0200

    Update Failure_reporter_using_err_stream.hpp

--- source/library/text-io/exports/std_streams/failure-reporting/Failure_reporter_using_err_stream.hpp
+++ source/library/text-io/exports/std_streams/failure-reporting/Failure_reporter_using_err_stream.hpp
@@ -19,10 +19,10 @@ namespace fabulous_support_machinery::std_streams::_definitions {
     {
         void output( in_<string_view> text ) const override
         {
+            for( const auto id: { Stream_id::out, Stream_id::err } ) { flush( id ); }
             if( is_console_stream( Stream_id::out ) and is_console_stream( Stream_id::err ) ) {
                 output( "\n" );
             }
-            for( const auto id: { Stream_id::out, Stream_id::err } ) { flush( id ); }
             output_err( text, "\n" );
             flush( Stream_id::err );
         }
