3d2a2637046c1dcc44d8fa4a6b3ef66ce0de5c4f
alf-p-steinbach <alf.p.steinbach@gmail.com>
2023-05-07 13:25:24 +0200

    Update asserted.utf8-literals.hpp

--- source/library/core/exports/+std-cpp-language/asserted.utf8-literals.hpp
+++ source/library/core/exports/+std-cpp-language/asserted.utf8-literals.hpp
@@ -1,7 +1,8 @@
 ﻿#pragma once    // Source encoding: UTF-8 with BOM (π is a lowercase Greek "pi").
 #include "utf8_is_the_execution_character_set.hpp"
 
-// There is unfortunately no known portable way to stop the compilation if this assert fires.
+// There is unfortunately no known portable way to stop the compilation if this assert fires,
+// and there is apparently no way to detect UTF-8 as execution character set in preprocessing.
 static_assert(
     fabulous_support_machinery::utf8_is_the_execution_character_set(),
     "The execution character set must be UTF-8 (e.g. MSVC option \"/utf-8\")."
