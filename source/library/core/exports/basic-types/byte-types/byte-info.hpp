fc76d78f447b86cb086696e687cd9e3b4bb0e2f4
alf-p-steinbach <alf.p.steinbach@gmail.com>
2023-05-07 13:30:04 +0200

    Update byte-info.hpp

--- source/library/core/exports/basic-types/byte-types/byte-info.hpp
+++ source/library/core/exports/basic-types/byte-types/byte-info.hpp
@@ -9,5 +9,5 @@ namespace fabulous_support_machinery {
     constexpr int bits_per_byte = CHAR_BIT;
     FSM_STATIC_ASSERT( bits_per_byte == 8 );    // Compilers with byte != octet are rare but do exist!
 
-    constexpr int max_byte_value = (1 << bits_per_byte) - 1;            // 255 for an octet, all bits 1.
+    constexpr int max_byte_value = static_cast<unsigned char>( -1 );    // 255 for an octet, all bits 1.
 }  // namespace fabulous_support_machinery
