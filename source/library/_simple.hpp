b709254bedfee91157c2b25c978fc866a3b3f8b5
alf-p-steinbach <alf.p.steinbach@gmail.com>
2023-05-09 22:44:21 +0200

    Update _simple.hpp

--- source/library/_simple.hpp
+++ source/library/_simple.hpp
@@ -5,10 +5,3 @@
 #include <fsm/$-macros.hpp>
 
 namespace fsm = fabulous_support_machinery;
-
-namespace fabulous_support_machinery::namespace_names {
-    $nsname_from( fsm, console );  $nsname_from( fsm, std_streams );  $nsname_from( fsm, fmtlib );
-    $nsname_from( fsm, u8 );  $nsname_from( fsm, unicode );
-    $nsname_from( fsm, iterable_for );
-    $nsname_from( fsm, stack_ops );
-}  // namespace fabulous_support_machinery::namespace_names
