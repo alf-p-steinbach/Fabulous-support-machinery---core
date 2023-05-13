e378e95381c48dcf3b97802f5b7d33768d810a39
alf-p-steinbach <alf.p.steinbach@gmail.com>
2023-05-13 21:28:42 +0200

    iterator classification functions

--- source/library/core/exports/misc.hpp
+++ source/library/core/exports/misc.hpp
@@ -2,6 +2,7 @@
 #include <fsm/core/exports/+std-cpp-language.hpp>
 
 #include <fsm/core/exports/misc/Default_value.hpp>
+#include <fsm/core/exports/misc/iterator-classification.hpp>
 #include <fsm/core/exports/misc/Literal_null.hpp>
 #include <fsm/core/exports/misc/support-for-collections.hpp>
 #include <fsm/core/exports/misc/support-for-threading.hpp>
--- source/library/core/exports/text/encoding/u8/Code_point_inspectors_mixin_.hpp
+++ source/library/core/exports/text/encoding/u8/Code_point_inspectors_mixin_.hpp
@@ -2,21 +2,22 @@
 #include <fsm/core/exports/+std-cpp-language.hpp>
 
 #include <fsm/core/exports/failure/detecting/FSM_STATIC_ASSERT.hpp>             // FSM_STATIC_ASSERT
+#include <fsm/core/exports/misc/iterator-classification.hpp>                    // is_tagged_as_forward_
 #include <fsm/core/exports/mixins/Relational_operators_mixin_.hpp>              // Relational_operators_mixin_
 #include <fsm/core/exports/text/encoding/u8/basic-sequence-functions.hpp>       // u8::*
 
 // #include <algorithm>            // std::min
-#include <iterator>             // std::iterator_traits
+#include <iterator>             // std::iterator_traits, std::next
 #include <string>               // std::string
 #include <string_view>          // std::string_view
 
 #include <string.h>             // memcmp
 
 namespace fabulous_support_machinery::u8::_definitions {
-    using   std::min,                   // <algorithm>
-            std::iterator_traits,       // <iterator>
-            std::string,                // <string>
-            std::string_view;           // <string_view>
+    using   std::min,                                           // <algorithm>
+            std::iterator_traits, std::next,                    // <iterator>
+            std::string,                                        // <string>
+            std::string_view;                                   // <string_view>
 
     template< class > struct Unit_iterator_of_t_;
 
@@ -29,6 +30,8 @@ namespace fabulous_support_machinery::u8::_definitions {
     {
     public:
         using Unit_iterator     = Unit_iterator_of_<Derived>;
+        FSM_STATIC_ASSERT( is_tagged_as_forward_<Unit_iterator> );
+        
         using Unit              = typename iterator_traits<Unit_iterator>::value_type;
         FSM_STATIC_ASSERT( sizeof( Unit ) == 1 );
 
@@ -44,14 +47,15 @@ namespace fabulous_support_machinery::u8::_definitions {
 
         auto n_bytes() const    -> int              { return lead_bytes::sequence_length_of( Byte( first_unit() ) ); }
         auto begin() const      -> Unit_iterator    { return it_start(); }
-        auto end() const        -> Unit_iterator    { return it_start() + n_bytes(); }
-        
+        auto end() const        -> Unit_iterator    { return next( it_start(), n_bytes() ); }
+
+        auto str() const        -> string           { return string( it_start(), n_bytes() ); }
+
+        // Requires contiguous iterator:
         auto unit_pointer() const -> const Unit*    { return &*it_start(); }
         auto char_pointer() const -> const char*    { return reinterpret_cast<const char*>( unit_pointer() ); }
-
         auto as_number() const  -> char32_t         { return byte_sequences::to_codepoint( it_start() ); }
         auto sv() const         -> string_view      { return string_view( char_pointer(), n_bytes() ); }
-        auto str() const        -> string           { return string( char_pointer(), n_bytes() ); }
     };
 
     template< class Derived >
@@ -62,9 +66,14 @@ namespace fabulous_support_machinery::u8::_definitions {
     {
         // UTF-8 sequences compare lexicographically just like their Unicode code points.
         // If one sequence is shorter then there's a difference within the shorter length.
-        return ::memcmp(
-            a.unit_pointer(), b.unit_pointer(), min( a.n_bytes(), b.n_bytes() )
-            );
+        const bool bytes_are_contiguous = true;     // TODO: check via iterator properties
+        if constexpr( bytes_are_contiguous ) {
+            return ::memcmp(
+                a.unit_pointer(), b.unit_pointer(), min( a.n_bytes(), b.n_bytes() )
+                );
+        } else {
+            throw 666;      // TODO
+        }
     }
 
     template< class Derived >
