87b8e64756c8c035f1e9e9c7ad70aa36d937a455
alf-p-steinbach <alf.p.steinbach@gmail.com>
2024-09-18 07:10:49 +0200

    -

---
+++ source/include/core/constructs/FSM_NSNAME_FROM.hpp
@@ -0,0 +1,17 @@
+﻿#pragma once    // Source encoding: UTF-8 with BOM (π is a lowercase Greek "pi").
+#include <fsm/core/std_core_language.hpp>
+//
+// Example:
+//
+//      FSM_NSNAME_FROM( std, chrono );
+//
+// Or with $-macros:
+//
+//      $nsname_from( std, chrono );
+
+#define FSM_NSNAME_FROM( containing_ns, name ) \
+    namespace name = containing_ns::name
+
+#ifndef FSM_NO_DOLLAR_MACROS_PLEASE
+#   define  $nsname_from    FSM_NSNAME_FROM
+#endif
