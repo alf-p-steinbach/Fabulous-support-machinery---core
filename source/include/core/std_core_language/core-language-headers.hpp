23e8003d75bc0c23579c656b246eed8966d6a65a
alf-p-steinbach <alf.p.steinbach@gmail.com>
2024-09-18 06:29:35 +0200

    -

--- source/include/core/std_core_language/core-language-headers.hpp
+++ source/include/core/std_core_language/core-language-headers.hpp
@@ -2,9 +2,9 @@
 
 #include <fsm/core/platform/cpp_version_macros.hpp>     // FSM_CPP_VERSION etc.
 
-#include <fsm/core/c/assert.hpp>    // assert
-#include <fsm/core/c/stddef.hpp>    // size_t and ptrdiff_t.
-#include <fsm/core/c/stdlib.hpp>    // EXIT_FAILURE, EXIT_SUCCESS.
+#include <fsm/core/wrapped/c_lib/assert.hpp>    // assert
+#include <fsm/core/wrapped/c_lib/stddef.hpp>    // size_t and ptrdiff_t.
+#include <fsm/core/wrapped/c_lib/stdlib.hpp>    // EXIT_FAILURE, EXIT_SUCCESS.
         
 #include <initializer_list>         // std::initializer_list, for use in range based `for`.
 #include <new>                      // std::operator new, for placement `new` expressions.
