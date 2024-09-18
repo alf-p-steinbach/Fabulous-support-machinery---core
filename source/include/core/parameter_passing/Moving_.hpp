a8410472a7bc40dfd9651e399bdec21d4089c0e2
alf-p-steinbach <alf.p.steinbach@gmail.com>
2024-09-18 08:19:13 +0200

    Update Moving_.hpp

--- source/include/core/parameter_passing/Moving_.hpp
+++ source/include/core/parameter_passing/Moving_.hpp
@@ -14,16 +14,19 @@ namespace fsm_definitions {
         
     public:
         template< class Arg >
-        Moving_( Arg&& arg ): m_object( forward( arg ) ) {}
+        Moving_( Arg&& arg ): m_object( forward<Arg>( arg ) ) {}
         
         Moving_( Moving_& other ): m_object( move( other.m_object ) ) {}
         Moving_( Moving_&& other ): m_object( move( other.m_object ) ) {}
         
         operator Type&& () noexcept { return move( m_object ); }
+
+        operator Type () const = delete;
+        operator const Type () const = delete;
     };
     
     FSM_( "EXPORTS:" ) namespace d = fsm_definitions;  namespace exports {
-        using d::in_;
+        using d::Moving_;
     }  // namespace exports
 }  // namespace fsm_definitions
 
