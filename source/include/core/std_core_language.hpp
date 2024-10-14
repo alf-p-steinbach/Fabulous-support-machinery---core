ce2e7359726d4c9fefad4bfaf7961683ba6fd23a
alf-p-steinbach <alf.p.steinbach@gmail.com>
2024-10-15 00:30:57 +0200

    -

--- source/include/core/std_core_language.hpp
+++ source/include/core/std_core_language.hpp
@@ -2,13 +2,13 @@
 
 //------------------------------------------------------------------------- Requirements:
 // TODO: assert minimum language version?
-#include <fsm/core/assertions/literals_are_utf8.hpp>                    // Encoding.
-#include <fsm/core/assertions/int_is_more_than_16_bits.hpp>             // `int` number range.
+#include <fsm/core/assertions/literals_are_utf8.hpp>                        // Encoding.
+#include <fsm/core/assertions/int_is_more_than_16_bits.hpp>                 // `int` number range.
 
 //------------------------------------------------------------------------- Enabling:
-#include <fsm/core/std_core_language/enable-std-literals.hpp>           // Syntax.
-#include <fsm/core/std_core_language/enable-alternate-keywords.hpp>     // Syntax.
-#include <fsm/core/std_core_language/core-language-headers.hpp>         // Semantics.
+#include <fsm/core/std_core_language/enable-std-literals.hpp>               // Syntax.
+#include <fsm/core/std_core_language/enable-alternate-keywords.hpp>         // Syntax.
+#include <fsm/core/std_core_language/enable-core-language-semantics.hpp>    // Semantics.
 
 //------------------------------------------------------------------------- Disabling:
 #ifdef __clang__
