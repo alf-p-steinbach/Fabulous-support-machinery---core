f5fd36aa855ee78f985c56096de96c9b34a60022
alf-p-steinbach <alf.p.steinbach@gmail.com>
2024-10-24 12:22:42 +0200

    -

---
+++ source/include/core/constructs/introspection/exception_type_name_for.hpp
@@ -0,0 +1,42 @@
+#pragma once    // Source encoding: UTF-8 with BOM (π is a lowercase Greek "pi").
+#include <fsm/core/platform/std_core_language.hpp>
+
+#include <fsm/core/constructs/introspection/name_of.hpp>        // name_of
+#include <fsm/core/type_builders.hpp>                           // in_
+
+#include <exception>
+#include <stdexcept>
+#include <string>
+
+namespace fsm_definitions {
+    using   fsm::name_of,
+            fsm::in_;                   // parameter_passing/in_
+    using   std::nested_exception,      // <exception>
+            std::exception,             // <stdexcept>
+            std::string;                // <string>
+
+    namespace introspection {
+
+        inline auto exception_type_name_for( in_<exception> x )
+            -> string
+        {
+            const string raw_name = name_of( typeid( x ) );
+            const bool has_nested_x = !!dynamic_cast<const nested_exception*>( &x );
+            if( has_nested_x ) {
+                const size_t    i_first     = raw_name.find_first_of( '<' ) + 1;
+                const size_t    i_beyond    = raw_name.find_last_of( '>' );
+                const auto      npos        = size_t( -1 );
+
+                if( i_first != npos and i_beyond != npos ) {
+                    return raw_name.substr( i_first, i_beyond - i_first );
+                }
+            }
+            return raw_name;
+        }
+
+    }  // namespace introspection
+}  // namespace fsm_definitions
+
+namespace fsm {
+    inline namespace introspection { using namespace fsm_definitions::introspection; }
+}  // namespace fsm
--- source/include/core/exception_handling/with_exceptions_displayed.hpp
+++ source/include/core/exception_handling/with_exceptions_displayed.hpp
@@ -1,18 +1,19 @@
 #pragma once    // Source encoding: UTF-8 with BOM (π is a lowercase Greek "pi").
 #include <fsm/core/platform/std_core_language.hpp>
 
+#include <fsm/core/constructs/introspection/exception_type_name_for.hpp>
 #include <fsm/core/exception_handling/for_each_exception_in.hpp>    // for_each_exception_in
 #include <fsm/core/parameter_passing/in_.hpp>                       // in_
 #include <fsm/core/stream_io/put.hpp>                               // put
 
 #include <functional>
 #include <stdexcept>
-#include <typeinfo>     // TODO: remove
 
 #include <fsm/core/wrapped/c_lib/stdlib.hpp>
 
 namespace fsm_definitions {
-    using   fsm::in_,                   // parameter_passing/in_
+    using   fsm::exception_type_name_for,
+            fsm::in_,                   // parameter_passing/in_
             fsm::put, fsm::put_to;      // stream_io/put
 
     using   std::function,              // <functional>
@@ -28,7 +29,9 @@ namespace fsm_definitions {
             } catch( in_<exception> x0 ) {
                 put( "\n" );    // TODO: restrict to stdout = console
                 for_each_exception_in( x0, [&]( in_<exception> x ) {
-                    put_to( stderr, "{}{} [ {} ]\n", (&x == &x0? "!" : "    because: "), x.what(), typeid( x ).name() );
+                    put_to( stderr, "{}{} [{}]\n",
+                        (&x == &x0? "!" : "    because: "), x.what(), exception_type_name_for( x )
+                        );
                 } );
             } catch( ... ) {
                 put( "\n" );    // TODO: restrict to stdout = console
