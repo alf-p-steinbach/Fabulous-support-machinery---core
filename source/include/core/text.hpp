01cdd0383fc397601af7402e4c89a4a43b7d7d2b
alf-p-steinbach <alf.p.steinbach@gmail.com>
2024-10-28 03:26:51 +0100

    -

--- source/include/core/text.hpp
+++ source/include/core/text.hpp
@@ -2,5 +2,6 @@
 #include <fsm/core/platform/std_core_language.hpp>
 
 #include <fsm/core/text/classifiers.hpp>
+#include <fsm/core/text/encoding.hpp>
 #include <fsm/core/text/formatting.hpp>
 #include <fsm/core/text/trimming.hpp>
